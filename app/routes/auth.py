ef70ab02163cf122080bb344f666772e8c3ee596
Gilson Kelvin Arruda Sales <kelvinarruda810@gmail.com>
2026-02-18 18:09:01 -0300

    fix oauth2 login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.database.db import get_db
-from app.schemas.user import UserLogin
 from app.core.security import verify_password, create_access_token
 from app.models.user import User
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/login")
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    db_user = db.query(User).filter(User.email == form_data.username).first()
 
-    if not db_user or not verify_password(user.password, db_user.password):
+    if not db_user or not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
 
     token = create_access_token({"sub": str(db_user.id)})
